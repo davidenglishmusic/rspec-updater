b9de3fd10f508e6591d0a058be912b88072a39d6
davidenglishmusic <david@operaonfilm.com>
2017-09-28 13:38:18 -0300

    handles .any_instance cases

--- app.rb
+++ app.rb
@@ -12,6 +12,9 @@ end
 
 def replace_should(line)
   updated_line = case line
+                 when /.any_instance.should_receive/
+                   fragment = line.split("\t")[-1].scan(/(.*?).any_instance.should_receive/).flatten.first
+                   line.gsub(fragment + '.any_instance.should_receive', "expect_any_instance_of(#{fragment}).to receive")
                  when /.should_receive/
                    fragment = line.split("\t")[-1].scan(/(.*?).should_receive/).flatten.first
                    line.gsub(fragment + '.should_receive', "expect(#{fragment}).to receive")
@@ -31,6 +34,9 @@ def replace_stub(line)
   when /.stub_chain/
     fragment = line.split("\t")[-1].scan(/(.*?).stub_chain\(/).flatten.first
     line.gsub(fragment + '.stub_chain', "allow(#{fragment}).to receive_message_chain")
+  when /.any_instance.stub\(/
+    fragment = line.split("\t")[-1].scan(/(.*?).any_instance.stub/).flatten.first
+    line.gsub(fragment + '.any_instance.stub', "allow_any_instance_of(#{fragment}).to receive")
   when /.stub\(/
     fragment = line.split("\t")[-1].scan(/(.*?).stub/).flatten.first
     line.gsub(fragment + '.stub', "allow(#{fragment}).to receive")
