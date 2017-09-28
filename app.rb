0f736d8d4ff6637e7279933aeefc4c5c1f51924a
davidenglishmusic <david@operaonfilm.com>
2017-09-28 20:47:32 -0300

    extracts out an Updater class

--- app.rb
+++ app.rb
@@ -1,62 +1,12 @@
-require 'pry'
-
-def replace_bracketed_should(line)
-  case line
-  when /{ should_not /
-    line.gsub('{ should_not', '{ is_expected.not_to')
-  when /{ should /
-    line.gsub('{ should', '{ is_expected.to')
-  else line
-  end
-end
-
-def replace_should(line)
-  updated_line = case line
-                 when /.any_instance.should_receive/
-                   fragment = line.split("\t")[-1].scan(/(.*?).any_instance.should_receive/).flatten.first
-                   line.gsub(fragment + '.any_instance.should_receive', "expect_any_instance_of(#{fragment}).to receive")
-                 when /.should_receive/
-                   fragment = line.split("\t")[-1].scan(/(.*?).should_receive/).flatten.first
-                   line.gsub(fragment + '.should_receive', "expect(#{fragment}).to receive")
-                 when /.should_not /
-                   fragment = line.split("\t")[-1].scan(/(.*?).should_not /).flatten.first
-                   line.gsub(fragment + '.should_not', "expect(#{fragment}).not_to")
-                 when /.should /
-                   fragment = line.split("\t")[-1].scan(/(.*?).should /).flatten.first
-                   line.gsub(fragment + '.should', "expect(#{fragment}).to")
-                 else line
-                 end
-  updated_line.include?(' == ') ? updated_line.gsub!(' == ', ' eql ') : updated_line
-end
-
-def replace_stub(line)
-  case line
-  when /.stub_chain/
-    fragment = line.split("\t")[-1].scan(/(.*?).stub_chain\(/).flatten.first
-    line.gsub(fragment + '.stub_chain', "allow(#{fragment}).to receive_message_chain")
-  when /.any_instance.stub\(/
-    fragment = line.split("\t")[-1].scan(/(.*?).any_instance.stub/).flatten.first
-    line.gsub(fragment + '.any_instance.stub', "allow_any_instance_of(#{fragment}).to receive")
-  when /.stub\(/
-    fragment = line.split("\t")[-1].scan(/(.*?).stub/).flatten.first
-    line.gsub(fragment + '.stub', "allow(#{fragment}).to receive")
-  else line
-  end
-end
+require './lib/updater'
 
 path = ARGV.first
 line_index = 1
+@updater = Updater.new
 
 lines = IO.readlines(path).map do |line|
   old_line = line
-  updated_line = line
-  if line.include?('{ should')
-    updated_line = replace_bracketed_should(line)
-  elsif line.include?('.should')
-    updated_line = replace_should(line)
-  elsif line.include?('.stub')
-    updated_line = replace_stub(line)
-  end
+  updated_line = @updater.update_line(old_line)
 
   if old_line != updated_line
     puts "Replacing #{line_index}"
---
+++ lib/updater.rb
@@ -0,0 +1,59 @@
+class Updater
+  def update_line(line)
+    if line.include?('{ should')
+      replace_bracketed_should(line)
+    elsif line.include?('.should')
+      replace_should(line)
+    elsif line.include?('.stub')
+      replace_stub(line)
+    else
+      line
+    end
+  end
+
+  private
+
+  def replace_bracketed_should(line)
+    case line
+    when /{ should_not /
+      line.gsub('{ should_not', '{ is_expected.not_to')
+    when /{ should /
+      line.gsub('{ should', '{ is_expected.to')
+    else line
+    end
+  end
+
+  def replace_should(line)
+    updated_line = case line
+                   when /.any_instance.should_receive/
+                     fragment = line.split("\t")[-1].scan(/(.*?).any_instance.should_receive/).flatten.first
+                     line.gsub(fragment + '.any_instance.should_receive', "expect_any_instance_of(#{fragment}).to receive")
+                   when /.should_receive/
+                     fragment = line.split("\t")[-1].scan(/(.*?).should_receive/).flatten.first
+                     line.gsub(fragment + '.should_receive', "expect(#{fragment}).to receive")
+                   when /.should_not /
+                     fragment = line.split("\t")[-1].scan(/(.*?).should_not /).flatten.first
+                     line.gsub(fragment + '.should_not', "expect(#{fragment}).not_to")
+                   when /.should /
+                     fragment = line.split("\t")[-1].scan(/(.*?).should /).flatten.first
+                     line.gsub(fragment + '.should', "expect(#{fragment}).to")
+                   else line
+                   end
+    updated_line.include?(' == ') ? updated_line.gsub!(' == ', ' eql ') : updated_line
+  end
+
+  def replace_stub(line)
+    case line
+    when /.stub_chain/
+      fragment = line.split("\t")[-1].scan(/(.*?).stub_chain\(/).flatten.first
+      line.gsub(fragment + '.stub_chain', "allow(#{fragment}).to receive_message_chain")
+    when /.any_instance.stub\(/
+      fragment = line.split("\t")[-1].scan(/(.*?).any_instance.stub/).flatten.first
+      line.gsub(fragment + '.any_instance.stub', "allow_any_instance_of(#{fragment}).to receive")
+    when /.stub\(/
+      fragment = line.split("\t")[-1].scan(/(.*?).stub/).flatten.first
+      line.gsub(fragment + '.stub', "allow(#{fragment}).to receive")
+    else line
+    end
+  end
+end
