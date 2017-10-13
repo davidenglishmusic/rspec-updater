eb275fab1dab7e6363b8a7192dd9d48090ed88c5
davidenglishmusic <david@operaonfilm.com>
2017-10-13 07:41:01 -0300

    handles whitespace

--- lib/updater.rb
+++ lib/updater.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Updater
   def update_line(line)
     return line if line.lstrip[0] == '#'
@@ -23,19 +25,19 @@ class Updater
     updated_line =
       case line
       when /.any_instance.should_receive/
-        fragment = line.split("\t")[-1].scan(/(.*?).any_instance.should_receive/).flatten.first
+        fragment = line.lstrip.scan(/(.*?).any_instance.should_receive/).flatten.first
         line.gsub(fragment + '.any_instance.should_receive', "expect_any_instance_of(#{fragment}).to receive")
       when /.should_not_receive/
-        fragment = line.split("\t")[-1].scan(/(.*?).should_not_receive/).flatten.first
+        fragment = line.lstrip.scan(/(.*?).should_not_receive/).flatten.first
         line.gsub(fragment + '.should_not_receive', "expect(#{fragment}).not_to receive")
       when /.should_receive/
-        fragment = line.split("\t")[-1].scan(/(.*?).should_receive/).flatten.first
+        fragment = line.lstrip.scan(/(.*?).should_receive/).flatten.first
         line.gsub(fragment + '.should_receive', "expect(#{fragment}).to receive")
       when /.should_not /
-        fragment = line.split("\t")[-1].scan(/(.*?).should_not /).flatten.first
+        fragment = line.lstrip.scan(/(.*?).should_not /).flatten.first
         line.gsub(fragment + '.should_not', "expect(#{fragment}).not_to")
       when /.should /
-        fragment = line.split("\t")[-1].scan(/(.*?).should /).flatten.first
+        fragment = line.lstrip.scan(/(.*?).should /).flatten.first
         line.gsub(fragment + '.should', "expect(#{fragment}).to")
       else line
       end
@@ -45,13 +47,13 @@ class Updater
   def replace_stub(line)
     case line
     when /.stub_chain/
-      fragment = line.split("\t")[-1].scan(/(.*?).stub_chain\(/).flatten.first
+      fragment = line.lstrip.scan(/(.*?).stub_chain\(/).flatten.first
       line.gsub(fragment + '.stub_chain', "allow(#{fragment}).to receive_message_chain")
     when /.any_instance.stub\(/
-      fragment = line.split("\t")[-1].scan(/(.*?).any_instance.stub/).flatten.first
+      fragment = line.scan(/(.*?).any_instance.stub/).flatten.first
       line.gsub(fragment + '.any_instance.stub', "allow_any_instance_of(#{fragment}).to receive")
     when /.stub\(/
-      fragment = line.split("\t")[-1].scan(/(.*?).stub/).flatten.first
+      fragment = line.lstrip.scan(/(.*?).stub/).flatten.first
       line.gsub(fragment + '.stub', "allow(#{fragment}).to receive")
     else line
     end
