7c5c40d3443c979857341f497bffa77dddd985a8
davidenglishmusic <david@operaonfilm.com>
2018-01-13 22:02:27 -0400

    runs stub line check before should line check

--- lib/line_factory.rb
+++ lib/line_factory.rb
@@ -1,14 +1,14 @@
 require_relative 'line'
 require_relative 'bracketed_line'
-require_relative 'should_line'
 require_relative 'stub_line'
+require_relative 'should_line'
 
 class LineFactory
   def self.from(code)
     return Line.new(code) if code.lstrip[0] == '#'
     return BracketedLine.new(code) if code.include?('{')
-    return ShouldLine.new(code) if code.include?('.should')
     return StubLine.new(code) if code.include?('.stub')
+    return ShouldLine.new(code) if code.include?('.should')
     Line.new(code)
   end
 end
