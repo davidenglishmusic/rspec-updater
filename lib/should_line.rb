77a2bc66e34abb303463f886e7872c6ee44062d2
davidenglishmusic <david@operaonfilm.com>
2018-01-20 11:25:56 -0400

    removes equality operator conversion from should update

--- lib/should_line.rb
+++ lib/should_line.rb
@@ -3,12 +3,6 @@ require_relative 'line'
 class ShouldLine < Line
   def updated
     fragment = extract_fragment(@code, /(.*?).should /)
-    update_equality_comparison(@code.gsub(fragment + '.should', "expect(#{fragment}).to"))
-  end
-
-  private
-
-  def update_equality_comparison(line)
-    line.include?(' == ') ? line.gsub!(' == ', ' eql ') : line
+    @code.gsub(fragment + '.should', "expect(#{fragment}).to")
   end
 end
