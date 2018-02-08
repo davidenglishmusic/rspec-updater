7038d5c899c8e9431241cfad2d2746bf64a37ab5
davidenglishmusic <david@operaonfilm.com>
2018-02-08 19:47:57 -0400

    handles bracketed should and should not lines which start with a temporary variable

--- lib/bracketed_inside_should_not_line.rb
+++ lib/bracketed_inside_should_not_line.rb
@@ -1,11 +1,11 @@
 require_relative 'bracketed_line'
-require_relative 'should_line'
+require_relative 'should_not_line'
 
 class BracketedInsideShouldNotLine < BracketedLine
-  PATTERN = /{\s*should_not(\.|\ |\()/
+  PATTERN = /{\s*\|\S+\|\s*(.+?.should_not.+?)\s*}/
 
   def updated
     unbracketed_code = bracketed_fragment(PATTERN)
-    @code.gsub(unbracketed_code, ShouldLine.new(extract_fragment(@code, unbracketed_code)).updated)
+    @code.gsub(unbracketed_code, ShouldNotLine.new(extract_fragment(@code, unbracketed_code)).updated)
   end
 end
